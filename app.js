f77a2692956a71842f37dc2b88036f6f33149a46
Ali <saberi.ar.eng@gmail.com>
2015-09-22 14:01:45 -0400

    fix: routing is corrected
      Routing problem is solved

--- app.js
+++ app.js
@@ -6,11 +6,7 @@ app.engine('html', require('hogan-express'));
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'views')));
 
-app.get('/', function(req, res, next){
-	// res.send('<h2>Hello World!</h2>');
-	res.render('index', {title:'kos nagoo'});
-});
-// require('./routes/route.js')(express, app);
+require('./routes/route.js')(express, app);
 
 app.listen(3000, function(){
 	console.log('chatCAT is running');
