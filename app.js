d452a5b55c65b7d2717542202fd691f7d45ebf67
Ali <saberi.ar.eng@gmail.com>
2015-09-24 18:10:53 -0400

    refactor: ejs is applied
        Since, I came from front-end background and angularJS, jade, and handlebar is not something
    that I may like. So, I prefer hogan-express and ejs but hogan has {{}}s for data binding which cause some troubles, So, I refacotor codes into ejs and move from there from right now!wq

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var express = require('express'),
 	app = express(),
 	path = require('path');
-app.set('views', path.join(__dirname,'views'));
-app.engine('html', require('hogan-express'));
-app.set('view engine', 'html');
-app.use(express.static(path.join(__dirname, 'views')));
+// app.set('views', path.join(__dirname,'views'));
+// app.engine('html', require('ejs-express'));
+
+// set the view emgine to ejs
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 require('./routes/route.js')(express, app);
+// app.get('/', function(req, res, next){
+// 		res.render('index', {title: 'Welcome to the ChatCAT'});
+// 	});
+
+// app.get('/chatrooms', function(req, res, next){
+// 		res.render('chatrooms', {title: 'Chatrooms'});
+// 	})
 
 app.listen(3000, function(){
 	console.log('chatCAT is running');
