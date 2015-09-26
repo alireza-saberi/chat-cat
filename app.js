5cf348af0a8936871ea7e4916195175a7655b49b
Ali <saberi.ar.eng@gmail.com>
2015-09-26 11:57:19 -0400

    feat: add and test of the session
       session management is added and used.

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 var express = require('express'),
 	app = express(),
-	path = require('path');
+	path = require('path'),
+	cookieParser = require('cookie-parser'),
+	session = require('express-session');
 // app.set('views', path.join(__dirname,'views'));
 // app.engine('html', require('ejs-express'));
 
 // set the view emgine to ejs
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+app.use(session({secret:'AliSaberi'}));
 
 require('./routes/route.js')(express, app);
-// app.get('/', function(req, res, next){
-// 		res.render('index', {title: 'Welcome to the ChatCAT'});
-// 	});
-
-// app.get('/chatrooms', function(req, res, next){
-// 		res.render('chatrooms', {title: 'Chatrooms'});
-// 	})
 
 app.listen(3000, function(){
 	console.log('chatCAT is running');
--- routes/route.js
+++ routes/route.js
@@ -4,8 +4,20 @@ module.exports = function(express, app){
 	router.get('/', function(req, res, next){
 		res.render('index', {title: 'Welcome to the ChatCAT'});
 	})
+
 	router.get('/chatrooms', function(req, res, next){
 		res.render('chatrooms', {title: 'Chatrooms'});
 	})
+
+	router.get('/setcolor', function(req,res, next){
+		req.session.favColor = "Red";
+		res.send('Setting Favourite color!');
+	})
+
+	router.get('/getcolor', function(req,res, next){
+		req.session.favColor = "Red";
+		res.send('Favourite color: ' + (req.session.favColor==undefined ? "Not found":req.session.favColor));
+	})
+
 	app.use('/', router);
 }
