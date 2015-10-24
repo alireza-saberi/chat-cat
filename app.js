e76715c65a440502e6782842b9fbee4ba778a02a
Ali <saberi.ar.eng@gmail.com>
2015-10-24 18:56:28 -0400

    feat: feature configuration is done

--- app.js
+++ app.js
@@ -2,18 +2,34 @@ var express = require('express'),
 	app = express(),
 	path = require('path'),
 	cookieParser = require('cookie-parser'),
-	session = require('express-session');
-// app.set('views', path.join(__dirname,'views'));
-// app.engine('html', require('ejs-express'));
+	session = require('express-session'),
+	config = require('./config/config.js'),
+	connectMongo = require('connect-mongo')(session);
 
 // set the view emgine to ejs
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
-app.use(session({secret:'AliSaberi'}));
+var env = process.env.NODE_ENV || 'development';
+	if(env === 'development'){
+		// dev specific settings
+		app.use(session({secret:config.sessionSecret}));
+		//app.use(session({secret:'AliSaberi'}));
+	} else {
+		// production specific settings
+		//app.use(session({}));
+		app.use(session({
+			secret:config.sessionSecret,
+			store: new connect-mongo({
+				url: config.dbURL,
+				stringify: true
+			})
+		}));
+	}
 
 require('./routes/route.js')(express, app);
 
 app.listen(3000, function(){
-	console.log('chatCAT is running');
+	console.log('chatCAT is running on port 3000. Press ctrl+C to exit');
+	console.log('Mode is ' + env);
 });
